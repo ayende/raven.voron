5edf293fa25c726a13320b4c797fc9d977fe8a76
Arkadiusz Palinski <arek@ais.pl>
2013-09-04 08:28:04 +0200

    Passing test of UnmanagedBits

--- Voron.Tests/Util/UnmanagedBitsTests.cs
+++ Voron.Tests/Util/UnmanagedBitsTests.cs
@@ -12,32 +12,30 @@ namespace Voron.Tests.Util
 {
 	public class UnmanagedBitsTests
 	{
+		[Fact]
+		public void CanSetAndGetTheSameValues()
+		{
+			var bytes = new byte[UnmanagedBits.CalculateSizeInBytesForAllocation(128, 4096)];
 
-		//TODO arek
-		//[Fact]
-		//public void CanSetAndGetTheSameValues()
-		//{
-		//	var bytes = new int[UnmanagedBits.GetSizeOfIntArrayFor(128)];
+			unsafe
+			{
+				fixed (byte* ptr = bytes)
+				{
+					var bits = new UnmanagedBits(ptr, 0, bytes.Length * sizeof(byte), 128, 4096);
 
-		//	unsafe
-		//	{
-		//		fixed (int* ptr = bytes)
-		//		{
-		//			var bits = new UnmanagedBits(ptr, 128);
+					var modeFactor = new Random().Next(1, 7);
 
-		//			var modeFactor = new Random().Next(1, 7);
+					for (int i = 0; i < 128; i++)
+					{
+						bits.MarkPage(i, (i % modeFactor) == 0);
+					}
 
-		//			for (int i = 0; i < 128; i++)
-		//			{
-		//				bits[i] = (i % modeFactor) == 0;
-		//			}
-
-		//			for (int i = 0; i < 128; i++)
-		//			{
-		//				Assert.Equal(bits[i], (i % modeFactor) == 0);
-		//			}
-		//		}
-		//	}
-		//}
+					for (int i = 0; i < 128; i++)
+					{
+						Assert.Equal(bits.IsFree(i), (i % modeFactor) == 0);
+					}
+				}
+			}
+		}
 	}
 }
